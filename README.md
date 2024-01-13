fc29fa93dd7d36ca115c086dac73ddfc18ce0e80
Kapish-patel <kpatelkapish@gmail.com>
2024-01-12 20:01:26 -0800

    init project

--- README.md
+++
@@ -1,2 +0,0 @@
-# SipSnap
-SipSnap is a Boba community app, where users can share recipies, find nearby boba cafe and host boba tasting events.
---
+++ sipsnap/android/app/src/main/kotlin/com/example/sipsnap/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.sipsnap
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
