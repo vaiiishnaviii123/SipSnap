eadc4f2bcc53ffb0733759c46271e2282a81511c
Kapish-patel <kpatelkapish@gmail.com>
2024-02-02 19:58:00 -0800

    added flash screen

--- sipsnap/lib/main.dart
+++ sipsnap/lib/main.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
 import 'view/home_screen.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 
 void main() {
+  // this displays the splash screen for a few seconds before the app starts
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+
   runApp(
     const MyApp(),
   );
@@ -12,10 +17,29 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Sip Snap',
-      theme: ThemeData(),
-      home: const HomeScreen(),
+    // Wrap your MaterialApp with a FutureBuilder to handle the delay
+    return FutureBuilder(
+      future: Future.delayed(const Duration(seconds: 3)), // Adjust the duration as needed
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.done) {
+          // Remove the splash screen here
+          FlutterNativeSplash.remove();
+          
+          // Return your MaterialApp with the desired configuration
+          return MaterialApp(
+            title: 'Sip Snap',
+            theme: ThemeData(),
+            home: const HomeScreen(),
+          );
+        } else {
+          // Return a placeholder or loading screen if needed
+          return const Scaffold(
+            body: Center(
+              child: CircularProgressIndicator(),
+            ),
+          );
+        }
+      },
     );
   }
 }
