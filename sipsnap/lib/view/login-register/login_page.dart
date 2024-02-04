d969aead25f62db95a3dfda73124cfd64cf9d0a8
Kapish-patel <kpatelkapish@gmail.com>
2024-02-03 16:48:29 -0800

    login page completed

--- sipsnap/lib/view/login-register/login_page.dart
+++ sipsnap/lib/view/login-register/login_page.dart
@@ -10,7 +10,10 @@ class LoginPage extends StatefulWidget {
 
 class _LoginPageState extends State<LoginPage> {
 
+  // Add a GlobalKey for the form
+  final _formKey = GlobalKey<FormState>();
 
+  // Add a TextEditingController for the email and password fields
   TextEditingController emailController = TextEditingController();
   TextEditingController passwordController = TextEditingController();
 
@@ -24,108 +27,116 @@ class _LoginPageState extends State<LoginPage> {
       appBar: AppBar(
         title: const Text('Sip Snap'),
       ),
-      body: Column(
-        children: [
-          // Add a SizedBox to create space between the top of the screen and the first widget
-          const SizedBox(height: 20),
-          const Text(
-            'Welcome to Sip Snap',
-            style: TextStyle(
-              fontSize: 18,
-            ),
-          ),
-          
-          // Add a SizedBox to create space between the text and the button
-          const SizedBox(height: 20),
-
-          // add a logo
-          const Image(image: AssetImage('assets/sip_snap@1x.png'), height: 200, width: 200),
-
-
-          // Add a SizedBox to create space between the logo and the form
-          const SizedBox(height: 20),
-
-          // add a form with two text fields and a button
-          Container(
-            padding: const EdgeInsets.all(20),
-            child: Form(
-              child: Column(
-                children: [
-                  TextFormField(
-                    controller: emailController,
-                    validator: (value) => value!.isEmpty ? 'Please enter your email' : null,
-                    decoration: const InputDecoration(
-                      labelText: 'Email',
-                    ),
-                  ),
-                  TextFormField(
-                    controller: passwordController,
-                    validator: (value) => value!.isEmpty ? 'Please enter your password' : null,
-                    decoration: const InputDecoration(
-                      labelText: 'Password',
-                    ),
-                  ),
-                  
-                  // Add a SizedBox to create space between the form and the button
-                  const SizedBox(height: 30),
-
-                  // make a row with two buttons for login and register
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children:[
-                      ElevatedButton(
-                        onPressed: () {
-                          if (emailController.text == _email && passwordController.text == _password) {
-                            Navigator.pushNamed(context, '/home');
-                          } else {
-                            ScaffoldMessenger.of(context).showSnackBar(
-                              const SnackBar(
-                                content: Text('Invalid email or password'),
-                              ),
-                            );
-                          }
-                        },
-                        child: const Text('Log in'),
-                      ),
-                      ElevatedButton(
-                        onPressed: () {
-                          Navigator.pushNamed(context, '/register');
-                        },
-                        child: const Text('Register'),
-                      ),
-                    ],
-                  )
-                ],
+      body: SingleChildScrollView(
+        child:Column(
+          children: [
+            // Add a SizedBox to create space between the top of the screen and the first widget
+            const SizedBox(height: 20),
+            const Text(
+              'Welcome to Sip Snap',
+              style: TextStyle(
+                fontSize: 18,
               ),
             ),
-          ),
-          // add links to reste password for future use
-          TextButton(
-            onPressed: () {
-              ScaffoldMessenger.of(context).showSnackBar(
-                const SnackBar(
-                  content: Text('This feature is not available yet'),
-                ),
-              );
-            },
-            child: const Text('Forgot password?'),
-          ),
+            
+            // Add a SizedBox to create space between the text and the button
+            const SizedBox(height: 20),
 
-          // Add a SizedBox to create space between the form and the button
-          const SizedBox(height: 20),
+            // add a logo
+            const Image(image: AssetImage('assets/sip_snap@1x.png'), height: 200, width: 200),
 
-          // add links to login with google 
-          ElevatedButton(
-            onPressed: () {
-              ScaffoldMessenger.of(context).showSnackBar(
-                const SnackBar(
-                  content: Text('This feature is not available yet'),
+
+            // Add a SizedBox to create space between the logo and the form
+            const SizedBox(height: 20),
+
+            // add a form with two text fields and a button
+            Container(
+              padding: const EdgeInsets.all(20),
+              child: Form(
+                key: _formKey,
+                child: Column(
+                  children: [
+                    TextFormField(
+                      controller: emailController,
+                      validator: (value) => value!.isEmpty ? 'Please enter your email' : null,
+                      keyboardType: TextInputType.emailAddress,
+                      decoration: const InputDecoration(
+                        labelText: 'Email',
+                      ),
+                    ),
+                    TextFormField(
+                      controller: passwordController,
+                      validator: (value) => value!.isEmpty ? 'Please enter your password' : null,
+                      obscureText: true,
+                      keyboardType: TextInputType.visiblePassword,
+                      decoration: const InputDecoration(
+                        labelText: 'Password',
+                      ),
+                    ),
+                    
+                    // Add a SizedBox to create space between the form and the button
+                    const SizedBox(height: 30),
+
+                    // make a row with two buttons for login and register
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children:[
+                        ElevatedButton(
+                          onPressed: () {
+                            if (_formKey.currentState!.validate()) {
+                              if (emailController.text == _email && passwordController.text == _password) {
+                                Navigator.pushNamed(context, '/home');
+                              } else {
+                                ScaffoldMessenger.of(context).showSnackBar(
+                                  const SnackBar(
+                                    content: Text('Invalid email or password'),
+                                  ),
+                                );
+                              }
+                            }
+                          },
+                          child: const Text('Log in'),
+                        ),
+                        ElevatedButton(
+                          onPressed: () {
+                            Navigator.pushNamed(context, '/register');
+                          },
+                          child: const Text('Register'),
+                        ),
+                      ],
+                    )
+                  ],
                 ),
-              );
-            },
-            child: const Text('Log in with Google'),
-          ),
-        ],
+              ),
+            ),
+            // add links to reste password for future use
+            TextButton(
+              onPressed: () {
+                ScaffoldMessenger.of(context).showSnackBar(
+                  const SnackBar(
+                    content: Text('This feature is not available yet'),
+                  ),
+                );
+              },
+              child: const Text('Forgot password?'),
+            ),
+
+            // Add a SizedBox to create space between the form and the button
+            const SizedBox(height: 20),
+
+            // add links to login with google 
+            ElevatedButton(
+              onPressed: () {
+                ScaffoldMessenger.of(context).showSnackBar(
+                  const SnackBar(
+                    content: Text('This feature is not available yet'),
+                  ),
+                );
+              },
+              child: const Text('Log in with Google'),
+            ),
+          ],
+        ),
       ),
     );
   }
