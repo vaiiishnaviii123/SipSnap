9587b7e30a4596b7ed9888c8aa3bca9c15c62944
Kapish-patel <kpatelkapish@gmail.com>
2024-02-29 01:42:02 -0800

    test case for login

--- sipsnap/test/login_form_test.dart
+++ sipsnap/test/login_form_test.dart
@@ -1,48 +1,41 @@
-import 'package:firebase_core/firebase_core.dart';
+
+import 'package:firebase_auth_mocks/firebase_auth_mocks.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:mockito/mockito.dart';
+import 'package:mockito/annotations.dart';
 import 'package:provider/provider.dart';
 import 'package:sipsnap/view/login_register/login_form.dart';
-import 'mocks/mock_user_provider.dart';
+import 'package:sipsnap/view_model/user_provider.dart';
 
 
+@GenerateMocks([UserProvider])
 void main() {
-
-  setUpAll(() async{
-    await Firebase.initializeApp();
-  });
 
   group('LoginForm Widget Tests', () {
     testWidgets('Tapping on Login with Google button calls signInWithGoogle method', (WidgetTester tester) async {
+      var auth = MockFirebaseAuth();
+      UserProvider userProvider = UserProvider(auth);
 
-      // Create a mock UserProvider
-      final mockUserProvider = MockUserProvider();
-
-      // Build the widget
       await tester.pumpWidget(
         MultiProvider(
-         providers: [
-          ChangeNotifierProvider(create: (context) => mockUserProvider),
-         ],
+          providers: [
+            ChangeNotifierProvider(create: (context) => userProvider),
+          ],
           child: MaterialApp(
             home: Material(
               child: LoginForm(),
             ),
-          )
+          ),
         )
       );
 
-      // find the login with google button
-      final loginWithGoogleButton = find.byKey(const ValueKey('loginPage googleButton'));
-
-      // Tap on the Login with Google button
-      await tester.tap(loginWithGoogleButton);
       await tester.pumpAndSettle();
 
+      await tester.press(find.byKey(const Key('loginPage googleButton')));
 
-      // Verify that signInWithGoogle method is called
-      verify(mockUserProvider.signInWithGoogle()).called(1);
+      // verify that _current user is created but null because we are provideing fake auth object
+      expect(userProvider.currentUser, null);
+
     });
   });
 }
--- sipsnap/test/login_page_test.dart
+++ sipsnap/test/login_page_test.dart
@@ -1,126 +1,44 @@
 // Add a widget test for login page widget
+
+import 'package:firebase_auth_mocks/firebase_auth_mocks.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:provider/provider.dart';
-import 'package:sipsnap/main.dart';
-import 'package:flutter/material.dart';
-// import 'package:sipsnap/models/register_page_event.dart';
-// import 'package:sipsnap/view/login/login_page.dart';
-// import 'package:sipsnap/view/login_register/register_page.dart';
-// import 'package:sipsnap/view_model/register_page_provider.dart';
-
-
+import 'package:sipsnap/view/login_register/login_form.dart';
+import 'package:sipsnap/view/login_register/login_page.dart';
+import 'package:sipsnap/view_model/user_provider.dart';
 
 void main(){
-  // testWidgets("Invalid email should display snack bar with message Please enter a valid email", (widgetTester) async{
-  //
-  //   //setup
-  //   // Load the app
-  //   await widgetTester.pumpWidget(const MyApp());
-  //
-  //
-  //   // fill the email and password fields
-  //   await widgetTester.enterText(find.byKey(const Key('login email field')), 'asdasd.asd');
-  //   await widgetTester.enterText(find.byKey(const Key('login password field')), 'asd');
-  //
-  //   // execute
-  //   // tap the login button
-  //   await widgetTester.tap(find.byKey(const Key('loginPage loginButton')));
-  //   await widgetTester.pumpAndSettle(const Duration(seconds: 1));
-  //
-  //   // expect
-  //   // expect a snack bar to be shown with the message "Invalid email format"
-  //   expect(find.byType(SnackBar), findsOneWidget);
-  //   expect(find.text('Please enter a valid email'), findsOneWidget);
-  //   //cleanup
-  //   await widgetTester.pumpAndSettle();
-  // });
-  //
-  // testWidgets("Login should throw exception on not entering email and password", (widgetTester) async{
-  //   //setup
-  //   // Load the app
-  //   await widgetTester.pumpWidget(const MyApp());
-  //
-  //   // execute
-  //   // tap the login button
-  //   await widgetTester.tap(find.byKey(const Key('loginPage loginButton')));
-  //   await widgetTester.pumpAndSettle(const Duration(seconds: 1));
-  //
-  //   // expect
-  //   // expect to be on the same login page
-  //   expect(find.text('Please enter your email'), findsOneWidget);
-  //   await widgetTester.pumpAndSettle();
-  // });
-  //
-  // testWidgets("Register button navigates to /register route", (widgetTester) async {
-  //   // Build our app and trigger a frame.
-  //   await widgetTester.pumpWidget(const MyApp());
-  //
-  //   // Tap on the Register button.
-  //   await widgetTester.tap(find.byKey(const ValueKey('login page registerButton')));
-  //   await widgetTester.pumpAndSettle(const Duration(seconds: 1));
-  //
-  //   // Verify that the navigation has occurred.
-  //   expect(find.byType(RegisterPage), findsOneWidget);
-  //   await widgetTester.pumpAndSettle();
-  // });
-  //
-  // testWidgets("Forgot password button shows a Snackbar", (widgetTester) async {
-  //   // Build the widget tree
-  //   await widgetTester.pumpWidget(const MyApp());
-  //
-  //   // find the forgot password button and tap it
-  //   final forgotPasswordButton = find.byKey(const ValueKey('loginPage forgetPasswordButton'));
-  //
-  //   // tap the forgot password button
-  //   await widgetTester.press(forgotPasswordButton);
-  //   await widgetTester.pumpAndSettle(const Duration(seconds: 1));
-  //
-  //   // expect to be on same page
-  //   expect(find.byType(LoginPage), findsOneWidget);
-  // });
-  //
-  // testWidgets("Log in with Google button shows a Snackbar", (WidgetTester tester) async {
-  //   // Build our app and trigger a frame.
-  //   await tester.pumpWidget(const MyApp());
-  //
-  //   // Tap on the Log in with Google button.
-  //   await tester.tap(find.byKey(const ValueKey('loginPage googleButton')));
-  //
-  //   // Wait for the SnackBar to be shown.
-  //   await tester.pumpAndSettle(const Duration(seconds: 1));
-  //
-  //   // expect to be on same page
-  //   expect(find.byType(LoginPage), findsOneWidget);
-  // });
-  //
-  // testWidgets('on correct email and password login should work', (widgetTester) async {
-  //
-  //   final registerPageProvider = RegisterPageProvider();
-  //
-  //   await widgetTester.pumpWidget(
-  //     MaterialApp(
-  //       home: ChangeNotifierProvider(
-  //         create: (context) => registerPageProvider,
-  //         child: LoginPage(),
-  //       ),
-  //     ),
-  //   );
-  //
-  //   final event = RegisterPageEvent(
-  //     id: '1',
-  //     email: 'asd@asd.asd',
-  //     password: 'password',
-  //     confirmPassword: 'password',
-  //   );
-  //
-  //   registerPageProvider.addRegisterPageEvent(event);
-  //
-  //   bool result = registerPageProvider.searchUserCheckPassword('asd@asd.asd', 'password');
-  //
-  //   // expect result to be true
-  //   expect(result, true);
-  //
-  // });
+
+  testWidgets('LoginPage should contain welcome text, logo, and login form', (WidgetTester tester) async {
+
+    var auth = MockFirebaseAuth();
+    UserProvider userProvider = UserProvider(auth);
+
+    await tester.pumpWidget(
+      MultiProvider(providers: [
+        ChangeNotifierProvider(create: (context) => userProvider),
+      ],
+        child: const MaterialApp(
+          home: Material(
+            child: LoginPage(),
+          ),
+        ),
+      )
+    );
+
+    // pumpWidget 
+    await tester.pumpAndSettle();
+
+    // Verify the presence of welcome text
+    expect(find.text('Welcome to Sip Snap'), findsOneWidget);
+
+    // Verify the presence of the logo image
+    expect(find.byType(Image), findsOneWidget);
+
+    // Verify the presence of the login form
+    expect(find.byType(LoginForm), findsOneWidget);
+  });
 }
 
 
