f3cb50c3968f5209872e937e9ce03f7b29adf194
Kapish-patel <kpatelkapish@gmail.com>
2024-02-29 13:42:02 -0800

    added spacing and padding

--- sipsnap/lib/view/create_post/create_post_page.dart
+++ sipsnap/lib/view/create_post/create_post_page.dart
@@ -90,122 +90,131 @@ class _CreatePostPageState extends State<CreatePostPage> {
       child: Scaffold(
         backgroundColor: Color.fromRGBO(250, 238, 230, 1),
         body: SingleChildScrollView(
-        child: Wrap(
-        //crossAxisAlignment: CrossAxisAlignment.start,
-          runSpacing:20,
-        children: [
-          Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            DropdownMenu<String>(
-              key: const Key("Type"),
-              initialSelection: list.first,
-              controller: dropdownController,
-              requestFocusOnTap: true,
-              label: const Text('Select Post type'),
-              onSelected: (String? value) {
-                setState(() {
-                      dropdownValue = value!;
-                      if(dropdownValue == "Community Post"){
-                        message = "Community event Title";
-                        _isCommunityPost = true;
-                        _isRecipe = false;
-                      }else{
-                        message = "Recipe Title";
-                        _isRecipe = true;
-                        _isCommunityPost = false;
-                      }
-                    });
-              },
-              dropdownMenuEntries: list
-                  .map<DropdownMenuEntry<String>>(
-                      (String value) {
-                    return DropdownMenuEntry<String>(
-                      value: value,
-                      label: value,
-                    );
-                  }).toList(),
-            ),
-      ]),
-          if(_isRecipe)Center(child: const Text("Recipe Post.", style: TextStyle(color: Colors.black54, fontSize: 35))),
-          if(_isCommunityPost)Center(child: const Text("Event Post.", style: TextStyle(color: Colors.black54, fontSize: 35))),
-          //const Text("Title:", style: TextStyle(color: Colors.black54, fontSize: 25)),
-          TextFormField(
-            key: Key("Title"),
-            controller: titleController,
-            decoration: InputDecoration(
-             labelText: message,
-            ),
-            // The validator receives the text that the user has entered.
-            validator: (value) {
-              if (value == null || value.isEmpty) {
-                return 'Please enter a title.';
-              }
-              return null;
-            },
-          ),
-          const Text("Add Picture.", style: TextStyle(color: Colors.black54, fontSize: 20)),
-          // camera and gallery icon button
-          Container(
-            decoration: BoxDecoration(
-              border: Border.all(),
-              borderRadius: BorderRadius.all(Radius.circular(10)),
-            ),
-            child:  FilePicker(_setImageUrl, _setUrl),
-          ),
-          if(url.isNotEmpty)Container(
-            height: 400.0,
-            color: Colors.grey,
-            child: Image.file(File(url), fit: BoxFit.cover, width: double.infinity),
-          ),
-          if(_isRecipe)const Text("Ingredients and Method.", style: TextStyle(color: Colors.black54, fontSize: 20)),
-          if(_isCommunityPost)const Text("Description.", style: TextStyle(color: Colors.black54, fontSize: 20)),
-          TextFormField(
-            key: Key("Description"),
-            controller: descriptionController,
-            maxLines: 3,
-            decoration: InputDecoration(
-              border: OutlineInputBorder(),
-              alignLabelWithHint: true,
-              labelText: "Enter here."
-            ),
-            // The validator receives the text that the user has entered.
-            validator: (value) {
-              if (value == null || value.isEmpty) {
-                return 'Please enter the description.';
-              }
-              return null;
-            },
-          ),
-          Center(
-            child: ElevatedButton(
-              key: Key("SavePost"),
-              style: ElevatedButton.styleFrom(
-                minimumSize: Size(200, 50),
-                backgroundColor: Color.fromRGBO(253,220,198,1),
-                foregroundColor: Colors.black,
+          child:Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+
+              // add some space
+              const SizedBox(height: 20),
+              DropdownMenu<String>(
+                key: const Key("Type"),
+                initialSelection: list.first,
+                controller: dropdownController,
+                requestFocusOnTap: true,
+                label: const Text('Select Post type'),
+                onSelected: (String? value) {
+                  setState(() {
+                        dropdownValue = value!;
+                        if(dropdownValue == "Community Post"){
+                          message = "Community event Title";
+                          _isCommunityPost = true;
+                          _isRecipe = false;
+                        }else{
+                          message = "Recipe Title";
+                          _isRecipe = true;
+                          _isCommunityPost = false;
+                        }
+                      });
+                },
+                dropdownMenuEntries: list
+                    .map<DropdownMenuEntry<String>>(
+                        (String value) {
+                      return DropdownMenuEntry<String>(
+                        value: value,
+                        label: value,
+                      );
+                    }).toList(),
               ),
-              onPressed: () {
-                // Validate returns true if the form is valid, or false otherwise.
-                if (_formKey.currentState!.validate()) {
-                  // you'd often call a server or save the information in a database.
-                  _onSavePressed();
-                  ScaffoldMessenger.of(context).showSnackBar(
-                    const SnackBar(
-                      key: Key("SnackBar"),
-                      content: Text("Posted successfully!", style: TextStyle(color: Colors.black54, fontWeight: FontWeight.bold)),
-                      backgroundColor: Color.fromRGBO(232,200,114, 1),
-                    ),
-                  );
-                }
-                FocusScope.of(context).unfocus();
-              },
-              child: const Text('Save'),
-            ),
+              if(_isRecipe)Center(child: const Text("Recipe Post.", style: TextStyle(color: Colors.black54, fontSize: 35))),
+              if(_isCommunityPost)Center(child: const Text("Event Post.", style: TextStyle(color: Colors.black54, fontSize: 35))),
+              TextFormField(
+                key: Key("Title"),
+                controller: titleController,
+                decoration: InputDecoration(
+                labelText: message,
+                ),
+                // The validator receives the text that the user has entered.
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter a title.';
+                  }
+                  return null;
+                },
+              ),
+
+              // add some space
+              const SizedBox(height: 20),
+              const Text("Add Picture.", style: TextStyle(color: Colors.black54, fontSize: 20)),
+              // camera and gallery icon button
+              Container(
+                decoration: BoxDecoration(
+                  border: Border.all(),
+                  borderRadius: BorderRadius.all(Radius.circular(10)),
+                ),
+                child:  FilePicker(_setImageUrl, _setUrl),
+              ),
+              if(url.isNotEmpty)Container(
+                height: 400.0,
+                color: Colors.grey,
+                child: Image.file(File(url), fit: BoxFit.cover, width: double.infinity),
+              ),
+
+              // add some space
+              const SizedBox(height: 20),
+              if(_isRecipe)const Text("Ingredients and Method.", style: TextStyle(color: Colors.black54, fontSize: 20)),
+              if(_isCommunityPost)const Text("Description.", style: TextStyle(color: Colors.black54, fontSize: 20)),
+              TextFormField(
+                key: Key("Description"),
+                controller: descriptionController,
+                maxLines: 3,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  alignLabelWithHint: true,
+                  labelText: "Enter here."
+                ),
+                // The validator receives the text that the user has entered.
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter the description.';
+                  }
+                  return null;
+                },
+              ),
+
+              // add some space
+              const SizedBox(height: 20),
+              Center(
+                child: ElevatedButton(
+                  key: Key("SavePost"),
+                  style: ElevatedButton.styleFrom(
+                    minimumSize: Size(200, 50),
+                    backgroundColor: Color.fromRGBO(253,220,198,1),
+                    foregroundColor: Colors.black,
+                  ),
+                  onPressed: () {
+                    // Validate returns true if the form is valid, or false otherwise.
+                    if (_formKey.currentState!.validate()) {
+                      // you'd often call a server or save the information in a database.
+                      _onSavePressed();
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        const SnackBar(
+                          key: Key("SnackBar"),
+                          content: Text("Posted successfully!", style: TextStyle(color: Colors.black54, fontWeight: FontWeight.bold)),
+                          backgroundColor: Color.fromRGBO(232,200,114, 1),
+                        ),
+                      );
+                    }
+                    FocusScope.of(context).unfocus();
+                  },
+                  child: const Text('Save'),
+                ),
+              ), 
+
+              // add some space
+              const SizedBox(height: 20),
+            ],
           ),
-        ],
         ),
-    ),
       ),
     );
   }
